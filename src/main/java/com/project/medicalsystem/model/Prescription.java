1c30c87faabac4dadbf8003ec020a2f351c67673
k.m.m.m.karunadasa <it25101145@my.sliit.lk>
2026-05-15 11:23:03 +0530

    Prescription class implemented

--- src/main/java/com/project/medicalsystem/model/Prescription.java
+++ src/main/java/com/project/medicalsystem/model/Prescription.java
@@ -1,17 +1,22 @@
 package com.project.medicalsystem.model;
 
-
 public class Prescription extends BaseEntity {
 
     private String patientId;
-    private String patientName;
-    private String doctorId;
-    private String specialty;
-    private String medicine;
-    private String dosage;
-    private String usage;
-    private String notes;
 
+    private String patientName;
+
+    private String doctorId;
+
+    private String specialty;
+
+    private String medicine;
+
+    private String dosage;
+
+    private String usage;
+
+    private String notes;
     public Prescription(String id,
                         String patientId,
                         String patientName,
@@ -21,7 +26,7 @@ public class Prescription extends BaseEntity {
                         String dosage,
                         String usage,
                         String notes) {
-        super(id);
+        super(id);   
         this.patientId   = patientId;
         this.patientName = patientName;
         this.doctorId    = doctorId;
@@ -33,27 +38,34 @@ public class Prescription extends BaseEntity {
     }
 
     public String getPatientId()   { return patientId; }
+
     public String getPatientName() { return patientName; }
+
     public String getDoctorId()    { return doctorId; }
+
     public String getSpecialty()   { return specialty; }
+
     public String getMedicine()    { return medicine; }
+
     public String getDosage()      { return dosage; }
+
     public String getUsage()       { return usage; }
+
     public String getNotes()       { return notes; }
 
     public void setMedicine(String medicine) { this.medicine = medicine; }
+
     public void setDosage(String dosage)     { this.dosage = dosage; }
+
     public void setUsage(String usage)       { this.usage = usage; }
+
     public void setNotes(String notes)       { this.notes = notes; }
-
-
     @Override
     public String toFileString() {
         return id + "," + patientId + "," + patientName + "," +
                doctorId + "," + specialty + "," +
                medicine + "," + dosage + "," + usage + "," + notes;
     }
-
     @Override
     public String getDisplayInfo() {
         return "Patient: " + patientName +
@@ -61,8 +73,6 @@ public class Prescription extends BaseEntity {
                " | Medicine: " + medicine +
                " | Dosage: " + dosage;
     }
-
-
     public static Prescription fromString(String line) {
         try {
             String[] p = line.split(",");
